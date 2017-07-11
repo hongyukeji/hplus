91f32e662e7902d3c6ee36b545b70a8eb7c64645
Shadow <admin@hongyuvip.com>
2017-07-11 23:57:23 +0800

    HongYuKeJi Hplus

---
+++ assets/HplusAsset.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace hongyukeji\hplus;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class HplusAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/hongyukeji/ehplus/static/';
+    public $css = [
+        'css/bootstrap.min14ed.css?v=3.3.6',
+        'css/font-awesome.min93e3.css?v=4.4.0',
+        'css/animate.min.css',
+        'css/style.min862f.css?v=4.1.0',
+    ];
+    public $js = [
+        'js/jquery.min.js?v=2.1.4',
+        'js/bootstrap.min.js?v=3.3.6',
+        'js/plugins/metisMenu/jquery.metisMenu.js',
+        'js/plugins/slimscroll/jquery.slimscroll.min.js',
+        'js/plugins/layer/layer.min.js',
+        'js/hplus.min.js?v=4.1.0',
+        'js/contabs.min.js',
+        'js/plugins/pace/pace.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
